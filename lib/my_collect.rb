7c40d2a1001ff085056166cd6361961711cc1546
Zachary Farrell <zfarrell0507@gmail.com>
2020-06-28 18:55:44 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,11 @@
 languages = ['ruby', 'javascript', 'python', 'objective-c']
 students = ['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller']
-my_collect(languages) do |language|
+
+def my_collect(languages) do |language|
       language.upcase
     end
-def my_collect (languages)
+    
+def my_collect(languages)
   lang = []
   languages.each do |language|
     lang << language.split(" ").first
