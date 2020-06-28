72cb72ca3687fe9215e7a3053137b38bef92f2e4
Zachary Farrell <zfarrell0507@gmail.com>
2020-06-28 18:48:11 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,7 +3,8 @@ students = ['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller']
 def my_collect (languages)
   lang = []
   languages.each do |language|
-    lang << language.upcase.split(" ").first
+    lang << language.split(" ").first
+    lang.upcase
   end
   lang
 end
